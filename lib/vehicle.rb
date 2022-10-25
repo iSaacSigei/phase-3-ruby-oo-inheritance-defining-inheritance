592fddc2cb12ba6cdca80ab42c0c59898d6d866e
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-25 11:59:57 +0300

    Completed assignment

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,20 @@
+require "pry"
 class Vehicle
+    attr_accessor :wheel_size, :wheel_number
+    def initialize  wheel_size, wheel_number
+        @wheel_number=wheel_number
+        @wheel_size=wheel_size    
+    end
+    def go
+        "vrrrrrrrooom!"
+    end
+    def fill_up_tank
+        "filling up!"
+    end
 
+end
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
 end
